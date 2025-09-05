698c97bf58fc6f2d8d8616f62b3bfc1da14c5c6b
Omkar <omkarkolhe912@gmail.com>
2025-09-05 17:01:24 +0530

    stack and heap

--- BASICS/datatypes-summary.js
+++ BASICS/datatypes-summary.js
@@ -1,36 +1,60 @@
-// # Primitive Datatypes
+// // # Primitive Datatypes
 
-//  7 Types
-// string, number , boolean , null , undefined, symbol, BigInt
+// //  7 Types
+// // string, number , boolean , null , undefined, symbol, BigInt
 
-const score = 100;
-const scoreValue = 100.3;
+// const score = 100;
+// const scoreValue = 100.3;
 
-const isLoggedIn = false;
-const outsideTemp = null;
+// const isLoggedIn = false;
+// const outsideTemp = null;
 
-let userEmail;
+// let userEmail;
 
-const id = Symbol("123");
-const anotherId = Symbol("123");
+// const id = Symbol("123");
+// const anotherId = Symbol("123");
 
-console.log(id === anotherId); // not same rather the values in the strings are same
+// console.log(id === anotherId); // not same rather the values in the strings are same
 
-//const bigNumber = 45632563256325634521n; //BigInt
+// //const bigNumber = 45632563256325634521n; //BigInt
 
-// reference type(Non primitive)
+// // reference type(Non primitive)
 
-// arrays , objects , functions
+// // arrays , objects , functions
 
-const heroes = ["shaktiman", "naagraj", "doga"];
-//objects
-let myObj = {
-  name: "omkar",
-  age: 22,
+// const heroes = ["shaktiman", "naagraj", "doga"];
+// //objects
+// let myObj = {
+//   name: "omkar",
+//   age: 22,
+// };
+
+// const myFunction = function () {
+//   console.log("Hello World");
+// };
+
+// console.log(typeof myFunction);
+
+//*********************************************** */
+
+// Stack(Primitive) , Heap(non-primitive)
+
+let youtubeName = "Omkar";
+let anotherName = youtubeName;
+
+anotherName = "chaiAurCode";
+
+// console.log(youtubeName);
+// console.log(anotherName);
+
+let user1 = {
+  email: "user@gmail.com",
+  upi: "user@ybl",
 };
 
-const myFunction = function () {
-  console.log("Hello World");
-};
+let user2 = user1;
 
-console.log(typeof myFunction);
+user2.email = "omkar@gmail.com";
+
+console.log(user1.email);
+console.log(user2.email);
